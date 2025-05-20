3f9748c4d60b48b6daded8581be64cb80ec9e04a
Qyoh <speedrun7760@gmail.com>
2025-05-20 10:46:15 -0700

    add runs

---
+++ run.sh
@@ -0,0 +1,15 @@
+#sudo pacman -S waybar nvim qutebrowser rofi --noconfirm
+rm -rf $HOME/.config/waybar/
+cp -r $HOME/personal/dots/waybar/ $HOME/.config/waybar/
+
+rm -rf $HOME/.config/nvim/
+cp -r $HOME/personal/dots/nvim $HOME/.config/nvim/
+
+rm -rf $HOME/.config/qutebrowser/
+cp -r $HOME/personal/dots/qutebrowser $HOME/.config/qutebrowser/
+
+rm -rf $HOME/.config/rofi/
+cp -r $HOME/personal/dots/rofi $HOME/.config/rofi/
+
+rm -rf $HOME/.config/spicetify/
+cp -r $HOME/personal/dots/spicetify $HOME/.config/spicetify/
