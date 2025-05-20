b5e4a025bcad7c5bb0d73ab9cd7be366249e22d9
Qyoh <speedrun7760@gmail.com>
2025-05-20 14:01:49 -0700

    refactor: how many comments??

--- nvim/lua/qyoh/remap.lua
+++ nvim/lua/qyoh/remap.lua
@@ -15,7 +15,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>r", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<CR>")
