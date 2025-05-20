c3a23da32edcd0e3c784b0923d9f73f71ee2d652
Qyoh7 <speedrun7760@gmail.com>
2025-05-20 16:11:14 -0700

    fix nvim config - install packer

--- rund.sh
+++ rund.sh
@@ -1,12 +1,8 @@
-#sudo pacman -S waybar nvim qutebrowser rofi --noconfirm
 rm -rf $HOME/.config/waybar/
 cp -r $HOME/personal/dots/waybar/ $HOME/.config/waybar/
 
 rm -rf $HOME/.config/nvim/
 cp -r $HOME/personal/dots/nvim $HOME/.config/nvim/
-
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 rm -rf $HOME/.config/qutebrowser/
 cp -r $HOME/personal/dots/qutebrowser $HOME/.config/qutebrowser/
