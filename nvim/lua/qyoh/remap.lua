3880b9cff9bf49b8e1d348739dff4f995d1ccd2b
Qyoh <speedrun7760@gmail.com>
2025-05-21 13:40:52 -0700

    fix exp remap?

--- nvim/lua/qyoh/remap.lua
+++ nvim/lua/qyoh/remap.lua
@@ -1,4 +1,4 @@
-vim.keymap.set("n", "<leader>e", "<cmd>Exp<CR>")
+vim.keymap.set("n", "<leader>pv", "<cmd>Exp<CR>")
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
