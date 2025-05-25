07d4f349c2b0ae1b21a9fc63d673d66bdf88e8e9
Qyoh7 <speedrun7760@gmail.com>
2025-05-25 08:39:42 -0700

    ???

--- runs/rund.sh
+++ runs/rund.sh
@@ -32,6 +32,7 @@ fi
 
 if [ ! -d "$HOME/.local/share/nvim/site/pack/packer" ]; then
     git clone --depth 1 https://github.com/wbthomason/packer.nvim "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim"
+else
     echo "packer already installed"
 fi
 
