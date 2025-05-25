3ee731d76f3c109f643c4a0d17caf0c62a3f3081
Qyoh7 <speedrun7760@gmail.com>
2025-05-24 18:33:07 -0700

    better runs

--- runs/rund.sh
+++ runs/rund.sh
@@ -1,30 +1,35 @@
 rm -rf $HOME/.config/hypr/
-cp -r $HOME/personal/dots/hyprdesk/ $HOME/.config/hypr/
+cp -r $DEV_ENV/hyprdesk/ $HOME/.config/hypr/
 
 rm -rf $HOME/.config/waybar/
-cp -r $HOME/personal/dots/waybar/ $HOME/.config/waybar/
+cp -r $DEV_ENV/waybar/ $HOME/.config/waybar/
 
 rm -rf $HOME/.config/nvim/
-cp -r $HOME/personal/dots/nvim $HOME/.config/nvim/
+cp -r $DEV_ENV/nvim $HOME/.config/nvim/
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
  ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+mkdir ~/.local/scripts
+cp -r $DEV_ENV/runs/tmux-sessionizer ~/.local/scripts
 
 rm -rf $HOME/.config/ghostty
-cp -r $HOME/personal/dots/ghostty $HOME/.config/ghostty
+cp -r $DEV_ENV/ghostty $HOME/.config/ghostty
 
 rm $HOME/.zshrc $HOME/.tmux.conf
-cp $HOME/personal/dots/rcs/.zshrc $HOME
-cp $HOME/personal/dots/rcs/.tmux.conf $HOME
+cp $DEV_ENV/rcs/.zshrc $HOME
+cp $DEV_ENV/rcs/.tmux.conf $HOME
+
+mkdir ~/.local/scripts
+cp $DEV_ENV/runs/tmux-sessionizer-desktop ~/.local/scripts/tmux-sessionizer
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 rm -rf $HOME/.config/qutebrowser/
-cp -r $HOME/personal/dots/qutebrowser $HOME/.config/qutebrowser/
+cp -r $DEV_ENV/qutebrowser $HOME/.config/qutebrowser/
 
 rm -rf $HOME/.config/rofi/
-cp -r $HOME/personal/dots/rofi $HOME/.config/rofi/
+cp -r $DEV_ENV/rofi $HOME/.config/rofi/
 
 rm -rf $HOME/.config/spicetify/
-cp -r $HOME/personal/dots/spicetify $HOME/.config/spicetify/
+cp -r $DEV_ENV/spicetify $HOME/.config/spicetify/
 hyprctl reload
 hyprshade toggle blue-light-filter
