97e50bc7217b548a4efe8c8bc4cf382fa83a0d81
Qyoh7 <speedrun7760@gmail.com>
2025-05-28 07:55:15 -0700

    binding for hidden files

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,3 +4,9 @@ vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+vim.keymap.set('n', '<leader>df', function()
+  builtin.find_files({
+    prompt_title = "< Find Dotfiles >",
+    hidden = true,
+    no_ignore = true,  -- include files ignored by .gitignore, optional
+  })
