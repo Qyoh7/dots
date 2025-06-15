9062d9366f93a9eefd0dabfb0a978c700e38e97e
Qyoh7 <speedrun7760@gmail.com>
2025-06-15 14:23:34 -0700

    tried and failed to fix sign column

--- nvim/lua/qyoh/init.lua
+++ nvim/lua/qyoh/init.lua
@@ -3,3 +3,10 @@ require("qyoh.set")
 require("qyoh.packer")
 require("qyoh.lsp")
 require("qyoh.java")
+
+vim.api.nvim_create_autocmd("OptionSet", {
+  pattern = "signcolumn",
+  callback = function()
+    print("signcolumn was changed to:", vim.o.signcolumn)
+  end,
+})
--- nvim/lua/qyoh/set.lua
+++ nvim/lua/qyoh/set.lua
@@ -1,3 +1,6 @@
+vim.opt.signcolumn = "yes"
+vim.wo.signcolumn = "yes"
+print("signcolumn is now set to:", vim.o.signcolumn)
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -8,6 +11,13 @@ vim.opt.expandtab = true
 vim.opt.smartindent = true
 
 vim.o.showtabline = 2
+
+--vim.api.nvim_create_autocmd("BufWinEnter", {
+  --callback = function()
+    --vim.wo.signcolumn = "yes"
+  --end,
+--})
+
 
 vim.opt.wrap = false
 
@@ -28,3 +38,12 @@ vim.opt.updatetime = 50
 vim.g.mapleader = " "
 
 vim.opt.clipboard = 'unnamedplus'
+
+vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter", "BufEnter" }, {
+  group = vim.api.nvim_create_augroup("ForceSignColumn", { clear = true }),
+  callback = function()
+    vim.schedule(function()
+      vim.wo.signcolumn = "yes"
+    end)
+  end,
+})
