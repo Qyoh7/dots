f6c6913680a5b54b9b115224d179d21d19a0194a
Qyoh7 <speedrun7760@gmail.com>
2025-11-04 16:48:03 -0800

    stuff that will cause merge conflicts

--- nvim/lua/config/remap.lua
+++ nvim/lua/config/remap.lua
@@ -8,14 +8,16 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
+vim.api.nvim_set_keymap('n', '<leader>n', ':set relativenumber!<CR>', { noremap = true, silent = true })
+
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set("n", "x", "\"_x", { desc = "Delete char without yanking" })
-vim.keymap.set("n", "X", "\"_X", { desc = "Delete backwards char without yanking" })
-vim.keymap.set("n", "<Del>", "\"_x", { desc = "Delete char via DEL without yanking" })
+vim.keymap.set({"n", "v"}, "x", "\"_x", { desc = "Delete char without yanking" })
+vim.keymap.set({"n", "v"}, "X", "\"_X", { desc = "Delete backwards char without yanking" })
+vim.keymap.set({"n", "v"}, "<Del>", "\"_x", { desc = "Delete char via DEL without yanking" })
 
 vim.keymap.set({ "n", "v" }, "<leader>d", "\"_d")
 
