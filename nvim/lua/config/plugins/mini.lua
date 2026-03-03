90906dccbed1ece5ef5cd118021c3a8e5a1a6910
Qyoh7 <speedrun7760@gmail.com>
2026-03-03 10:12:34 -0800

    no more autopairs

--- nvim/lua/config/plugins/mini.lua
+++ nvim/lua/config/plugins/mini.lua
@@ -5,7 +5,7 @@ return {
         config = function()
             local statusline = require 'mini.statusline'
             statusline.setup { use_icons = true }
-	    require('mini.pairs').setup()
+-- 	    require('mini.pairs').setup()
 	    require('mini.move').setup()
         end
     }
