e9e4d5a3be0a8257bb5da05bd7f5e8583b7725ec
Qyoh7 <speedrun7760@gmail.com>
2026-08-23 09:24:52 -0700

    fix treesitter

--- common/.config/nvim/lua/plugins/plugins.lua
+++ common/.config/nvim/lua/plugins/plugins.lua
@@ -6,7 +6,18 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         lazy = false,
-        build = ':TSUpdate'
+        build = ':TSUpdate',
+        config = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function()
+                    local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+                    if lang and vim.treesitter.language.add(lang) then
+                        vim.treesitter.start()
+                    end
+                end,
+            })
+        end
+
     },
     {
         "ThePrimeagen/harpoon",
