dcf261042575d3fb0e22737a1bec55d1a22b6dd0
Qyoh7 <speedrun7760@gmail.com>
2026-08-24 12:58:15 -0700

    fix diagnostic bind

--- common/.config/nvim/lua/plugins/plugins.lua
+++ common/.config/nvim/lua/plugins/plugins.lua
@@ -37,6 +37,9 @@ return {
     { 
         "neovim/nvim-lspconfig",
         config = function()
+            vim.keymap.set('n', 'gl',  vim.diagnostic.open_float)
+            vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+            vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
             vim.keymap.set('n', 'K',   vim.lsp.buf.hover,         opts)
             vim.keymap.set('n', 'gd',  vim.lsp.buf.definition,    opts)
             vim.keymap.set('n', 'gD',  vim.lsp.buf.declaration,   opts)
